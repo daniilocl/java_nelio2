f1eec6019206788349c25d84ac6e6e872509ee9b
daniilocl <danilocarvalho.matos67@gmail.com>
2026-01-23 22:08:49 -0300

    Exercício de foreach

--- array/src/Foreach.java
+++ array/src/Foreach.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Foreach {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String[] vect = new String[] {"Maria", "Bob", "João"};
+
+        for (int i=0; i<vect.length; i++) {
+            System.out.println(vect[i]);
+        }
+
+        System.out.println("------------");
+
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+
+        sc.close();
+    }
 }
